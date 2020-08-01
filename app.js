61233839dcbb3f9adcca10ae1c3d73b7559a2221
shikhar <shikharswat64@gmail.com>
2020-08-01 16:37:32 +0530

    resolved bugs

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ app.use(
   })
 );
 
-// mongoose
-//   .connect(
-//     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.powbv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-//   )
-//   .then(() => {
-//     app.listen(port);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-app.listen(port);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.powbv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
